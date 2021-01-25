eb7d9de424a19c819944324e08f32948e14d6ab4
Rob <rob.rimmer@mclaren.com>
2021-01-25 09:29:02 +0000

    Upgrade console (breaking changes)

--- display/display.go
+++ display/display.go
@@ -2,9 +2,11 @@ package treedisplay
 
 import (
 	"fmt"
-	"github.com/atrico-go/console"
-	"github.com/atrico-go/tree"
 	"strings"
+
+	"github.com/atrico-go/console/box_drawing"
+
+	"github.com/atrico-go/tree"
 )
 
 func DisplayTree(root tree.Node, config DisplayTreeConfig) []string {
@@ -40,42 +42,44 @@ func formatNode(details nodeDetails) string {
 	// Attachment to tree structure
 	if !details.isRoot() {
 		if details.parentPosition == aboveParent && details.siblingLocation == firstSibling {
-			text.WriteString(console.MustGetBoxChar(false, true, false, true, console.BoxSingle.HeavyIf(details.isOnHighlightPath()))) // ┌
+			text.WriteRune(box_drawing.MustGetBoxChar(false, true, false, true, box_drawing.BoxSingle.HeavyIf(details.isOnHighlightPath()))) // ┌
 		} else if details.parentPosition == belowParent && details.siblingLocation == lastSibling {
-			text.WriteString(console.MustGetBoxChar(true, false, false, true, console.BoxSingle.HeavyIf(details.isOnHighlightPath()))) // └
+			text.WriteRune(box_drawing.MustGetBoxChar(true, false, false, true, box_drawing.BoxSingle.HeavyIf(details.isOnHighlightPath()))) // └
 		} else if details.isHighlighted() {
-			text.WriteString(console.MustGetBoxCharMixed(
-				console.BoxSingle.HeavyIf(details.parentPosition == belowParent),
-				console.BoxSingle.HeavyIf(details.parentPosition == aboveParent),
-				console.BoxNone,
-				console.BoxHeavy)) // ├
+			text.WriteRune(box_drawing.MustGetBoxCharMixed(box_drawing.BoxParts{
+				Up:    box_drawing.BoxSingle.HeavyIf(details.parentPosition == belowParent),
+				Down:  box_drawing.BoxSingle.HeavyIf(details.parentPosition == aboveParent),
+				Left:  box_drawing.BoxNone,
+				Right: box_drawing.BoxHeavy})) // ├
 		} else if details.isOnHighlightPath() {
-			text.WriteString(console.MustGetBoxCharMixed(
-				console.BoxSingle.HeavyIf(details.parentPosition == belowParent),
-				console.BoxSingle.HeavyIf(details.parentPosition == aboveParent),
-				console.BoxNone,
-				console.BoxHeavy)) // ├
+			text.WriteRune(box_drawing.MustGetBoxCharMixed(box_drawing.BoxParts{
+				Up:    box_drawing.BoxSingle.HeavyIf(details.parentPosition == belowParent),
+				Down:  box_drawing.BoxSingle.HeavyIf(details.parentPosition == aboveParent),
+				Left:  box_drawing.BoxNone,
+				Right: box_drawing.BoxHeavy})) // ├
 		} else {
 			passThrough := (details.highlightPosition == siblingHighlightAbove && details.parentPosition == aboveParent) ||
 				(details.highlightPosition == siblingHighlightBelow && details.parentPosition == belowParent)
-			text.WriteString(console.MustGetBoxCharMixed(
-				console.BoxSingle.HeavyIf(passThrough),
-				console.BoxSingle.HeavyIf(passThrough),
-				console.BoxNone,
-				console.BoxSingle)) // ├
+			text.WriteRune(box_drawing.MustGetBoxCharMixed(box_drawing.BoxParts{
+				Up:    box_drawing.BoxSingle.HeavyIf(passThrough),
+				Down:  box_drawing.BoxSingle.HeavyIf(passThrough),
+				Left:  box_drawing.BoxNone,
+				Right: box_drawing.BoxSingle})) // ├
 		}
 	}
 	// Dash
-	text.WriteString(console.MustGetBoxChar(false, false, true, true, console.BoxSingle.HeavyIf(details.isOnHighlightPath()))) // -
+	text.WriteRune(box_drawing.MustGetBoxChar(false, false, true, true, box_drawing.BoxSingle.HeavyIf(details.isOnHighlightPath()))) // -
 	// Name of node (and branch chars)
-	up := console.ConditionalBoxType(details.children.above > 0, console.BoxSingle.HeavyIf(details.hasHighlightChildAbove()), console.BoxNone)
-	down := console.ConditionalBoxType(details.children.below > 0, console.BoxSingle.HeavyIf(details.hasHighlightChildBelow()), console.BoxNone)
-	left := console.BoxSingle.HeavyIf(details.isOnHighlightPath())
+	parts := box_drawing.BoxParts{}
+	parts.Up = box_drawing.ConditionalBoxType(details.children.above > 0, box_drawing.BoxSingle.HeavyIf(details.hasHighlightChildAbove()), box_drawing.BoxNone)
+	parts.Down = box_drawing.ConditionalBoxType(details.children.below > 0, box_drawing.BoxSingle.HeavyIf(details.hasHighlightChildBelow()), box_drawing.BoxNone)
+	parts.Left = box_drawing.BoxSingle.HeavyIf(details.isOnHighlightPath())
 	value := details.node.NodeValue()
-	right := console.ConditionalBoxType(value != nil || len(details.node.Children()) == 0, console.BoxSingle.HeavyIf(details.isHighlighted()), console.BoxNone)
-	text.WriteString(console.MustGetBoxCharMixed(up, down, left, right))
+	parts.Right = box_drawing.ConditionalBoxType(value != nil || len(details.node.Children()) == 0, box_drawing.BoxSingle.HeavyIf(details.isHighlighted()), box_drawing.BoxNone)
+	text.WriteRune(box_drawing.MustGetBoxCharMixed(parts))
 	if value != nil {
-		text.WriteString(fmt.Sprintf("%s%v", console.MustGetBoxChar(false,false,false,false, console.BoxNone), value))
+		text.WriteRune(box_drawing.MustGetBoxChar(false, false, false, false, box_drawing.BoxNone))
+		text.WriteString(fmt.Sprintf("%v", value))
 	}
 	// TODO - Start
 	// text.WriteString(fmt.Sprintf(":par=%v,sib=%v,hl=%v,hlIx=%v", details.parentPosition, details.siblingLocation, details.highlightPosition, details.highlightChildIdx))
@@ -107,12 +111,12 @@ func formatPrefix(details *nodeDetails, context parentPosition) string {
 					(context == aboveParent && details.isOnHighlightPath() && details.parentPosition == belowParent) ||
 					(context == belowParent && details.hasHighlightSiblingBelow()) ||
 					(context == aboveParent && details.hasHighlightSiblingAbove())
-				prefix.WriteString(console.MustGetBoxChar(true, true, false, false, console.BoxSingle.HeavyIf(highlight))) // |
+				prefix.WriteRune(box_drawing.MustGetBoxChar(true, true, false, false, box_drawing.BoxSingle.HeavyIf(highlight))) // |
 			} else {
-				prefix.WriteString(console.MustGetBoxChar(false,false,false,false, console.BoxNone)) // space
+				prefix.WriteRune(box_drawing.MustGetBoxChar(false, false, false, false, box_drawing.BoxNone)) // space
 			}
 		}
-		prefix.WriteString(console.MustGetBoxChar(false,false,false,false, console.BoxNone)) // space
+		prefix.WriteRune(box_drawing.MustGetBoxChar(false, false, false, false, box_drawing.BoxNone)) // space
 	}
 	return prefix.String()
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/atrico-go/tree
 go 1.15
 
 require (
-	github.com/atrico-go/console v1.1.1
+	github.com/atrico-go/console v1.7.0
 	github.com/atrico-go/core v1.0.0 // indirect
-	github.com/atrico-go/testing v1.0.0
+	github.com/atrico-go/testing v1.0.2
 	github.com/segmentio/ksuid v1.0.3
 )
